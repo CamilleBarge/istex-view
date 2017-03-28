e8cad502f052b069a6d04143aa7b61f920215b15
Stéphane Gully <stephane.gully@gmail.com>
2017-03-28 22:52:14 +0200

    check ISTEX API is alive and list the 10 first documents with real ARKs on the homepage

--- www/src/components/home.jsx
+++ www/src/components/home.jsx
@@ -1,19 +1,28 @@
 import    React from 'react';
 import    IstexArkStatus from './istex-ark-status.jsx';
+import    IstexApiStatus from './istex-api-status.jsx';
 
 module.exports = React.createClass({
   displayName: 'Home',
 
+  getInitialState: function () {
+    return {
+      arks: [],
+      istexIds: []
+    };
+  },
+
   render: function () {
+    let self = this;
+    var arks = self.state.arks;
 
-    var arks = [
-      'ark:/12345/X04-NLP74VSW-8', // 1A9EA12F2FE30BF2CFE9C45932AACC6F32A61F4F
-      'ark:/12345/X02-PG09NZ97-Z', // 1C58FEE7BB7A1ABF1940261E3AEF615E8D4E80C7
-      'ark:/12345/X01-38XC4LP5-7', // 3E4EADD3F8EABA5BC1571851A27FFB24AAE4B9D5
-      'ark:/12345/X02-PDVQTSPL-Q', // 53A5856F93AA18A6A59474157FD32AF2F3E4DADB
-      'ark:/12345/X04-3VNN2FVV-6'  // 6C0BCC79DAC47D158C984ED85AB5F2E565111948
-    ];
-
+    // var arks = [
+    //   'ark:/12345/X04-NLP74VSW-8', // 1A9EA12F2FE30BF2CFE9C45932AACC6F32A61F4F
+    //   'ark:/12345/X02-PG09NZ97-Z', // 1C58FEE7BB7A1ABF1940261E3AEF615E8D4E80C7
+    //   'ark:/12345/X01-38XC4LP5-7', // 3E4EADD3F8EABA5BC1571851A27FFB24AAE4B9D5
+    //   'ark:/12345/X02-PDVQTSPL-Q', // 53A5856F93AA18A6A59474157FD32AF2F3E4DADB
+    //   'ark:/12345/X04-3VNN2FVV-6'  // 6C0BCC79DAC47D158C984ED85AB5F2E565111948
+    // ];
     return (
  
     <div className="container" role="main">
@@ -34,12 +43,11 @@ module.exports = React.createClass({
         <h1>Visualisation interactive des objets documentaires ISTEX</h1>
         <p className="lead">
           ISTEX VIEW c'est une page HTML5 par objet documentaire ISTEX. Chaque page permet de visualiser le document PDF augmenté d'interactivités contextuelles de type survol de la souris ou autre. Chaque page est citable via une URL de type ARK (ex : <a href="/ark:/12345/X01-38XC4LP5-7">https://view.istex.fr/ark:/12345/X01-38XC4LP5-7</a>)
-          <br/><br/>
-          Ci-dessous la liste des documents ISTEX proposés dans ce prototype qui proposera dans un premier temps un service de références bibliographiques interactives au survol de la souris.
         </p>
       </div>
 
       <IstexArkStatus />
+      <IstexApiStatus />
 
       <p>
         {arks.map((ark) =>
@@ -56,11 +64,38 @@ module.exports = React.createClass({
   },
 
   componentDidMount () {
+    let self = this;
+
     // to have tooltips cf http://getbootstrap.com/javascript/#tooltips-examples
     $(function () {
       $('[data-toggle="tooltip"]').tooltip()
       $('.container').popover()
     });
+
+
+    // request the istex-view config
+    fetch('/config.json').then(function (response) {
+      return response.json();
+    }).then(function (config) {
+
+      // fetch the first 10 istex documents and
+      // extract the ARKs and the istexIds
+      fetch(config.istexApiUrl + '/document/?q=*&output=id,ark&sid=istex-view').then(function (response) {
+        return response.json();
+      }).then(function (apiJson) {
+        var arks = apiJson.hits.map(function (hit) {
+          return hit.ark;
+        });
+        var istexIds = apiJson.hits.map(function (hit) {
+          return hit.id;
+        });
+        self.setState({arks: arks, istexIds: istexIds});
+      }).catch(function (err) {
+        self.setState({arks: [], istexIds: []});
+      });
+
+
+    });    
   },
 
 
---
+++ www/src/components/istex-api-status.jsx
@@ -0,0 +1,47 @@
+import    React from 'react';
+
+module.exports = React.createClass({
+  displayName: 'IstexApiStatus',
+
+  getInitialState: function () {
+    return {
+      // do not show warning as long as the ajax
+      // request is not finished
+      isAvailable: true
+    };
+  },
+
+  componentDidMount () {
+    let self = this;
+
+    // request the istex-view config
+    fetch('/config.json').then(function (response) {
+      return response.json();
+    }).then(function (config) {
+
+      // call istex-api to check if the server is ready
+      fetch(config.istexApiUrl + '/document/?q=*&output=id&sid=istex-view').then(function (response) {
+        self.setState({isAvailable: true});
+      }).catch(function (err) {
+        self.setState({isAvailable: false});
+      });
+
+
+    });
+
+  },
+
+  render: function () {
+    let self = this;
+    if (!self.state.isAvailable) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          ISTEX API is not listening. Please start it to be able to view ISTEX documents.
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
+});
