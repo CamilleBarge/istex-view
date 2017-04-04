f5c55ef76727213b33cc58cb2b57cbb2d4af9252
Stéphane Gully <stephane.gully@gmail.com>
2017-04-04 22:04:20 +0200

    add openurl stuff to the homepage and to the react router

--- www/src/components/home.jsx
+++ www/src/components/home.jsx
@@ -1,5 +1,4 @@
 import    React from 'react';
-import    IstexArkStatus from './istex-ark-status.jsx';
 import    IstexApiStatus from './istex-api-status.jsx';
 
 module.exports = React.createClass({
@@ -14,15 +13,8 @@ module.exports = React.createClass({
 
   render: function () {
     let self = this;
-    var arks = self.state.istexIds;
+    var istexIds = self.state.istexIds;
 
-    // var arks = [
-    //   'ark:/12345/X04-NLP74VSW-8', // 1A9EA12F2FE30BF2CFE9C45932AACC6F32A61F4F
-    //   'ark:/12345/X02-PG09NZ97-Z', // 1C58FEE7BB7A1ABF1940261E3AEF615E8D4E80C7
-    //   'ark:/12345/X01-38XC4LP5-7', // 3E4EADD3F8EABA5BC1571851A27FFB24AAE4B9D5
-    //   'ark:/12345/X02-PDVQTSPL-Q', // 53A5856F93AA18A6A59474157FD32AF2F3E4DADB
-    //   'ark:/12345/X04-3VNN2FVV-6'  // 6C0BCC79DAC47D158C984ED85AB5F2E565111948
-    // ];
     return (
  
     <div className="container" role="main">
@@ -42,17 +34,19 @@ module.exports = React.createClass({
       <div className="jumbotron">
         <h1>Visualisation interactive des objets documentaires ISTEX</h1>
         <p className="lead">
-          ISTEX VIEW c'est une page HTML5 par objet documentaire ISTEX. Chaque page permet de visualiser le document PDF augmenté d'interactivités contextuelles de type survol de la souris ou autre. Chaque page est citable via une URL de type ARK (ex : <a href="/ark:/12345/X01-38XC4LP5-7">https://view.istex.fr/ark:/12345/X01-38XC4LP5-7</a>)
+          ISTEX VIEW c'est une page HTML5 par objet documentaire ISTEX. Chaque page permet de visualiser le document PDF augmenté d'interactivités contextuelles de type survol de la souris ou autre. Chaque page pointe vers un objet documentaire ISTEX. Exemple : <a href="/4B64F4937471EA91F40AABC46B2D4CD5D5CC304E">https://view.istex.fr/4B64F4937471EA91F40AABC46B2D4CD5D5CC304E</a><br/>(les ARK seront prochainement supportés)
         </p>
+        <p className="lead">
+          ISTEX VIEW c'est aussi une interface HTML au dessus de l'OpenURL de l'API ISTEX. Exemple: <a href="/openurl?rft_id=info:doi/10.1136/acupmed-2012-010183">https://view.istex.fr/openurl?rft_id=info:doi/10.1136/acupmed-2012-010183</a>
+        </p>        
       </div>
 
-      <IstexArkStatus />
       <IstexApiStatus />
 
       <p>
-        {arks.map((ark) =>
-          <a href={'/' + ark}  className="btn btn-default btn-lg" role="button">
-            <span className="glyphicon glyphicon-file" aria-hidden="true"></span> <code>{ark}</code>
+        {istexIds.map((istexid) =>
+          <a href={'/' + istexid}  className="btn btn-default btn-lg" role="button">
+            <span className="glyphicon glyphicon-file" aria-hidden="true"></span> <code>{istexid}</code>
           </a>
         )}
       </p>
--- www/src/main.jsx
+++ www/src/main.jsx
@@ -1,13 +1,15 @@
-import    React from 'react';
-import ReactDOM from 'react-dom';
-import ViewDoc  from './components/view-doc.jsx';
-import Home     from './components/home.jsx';
+import    React    from 'react';
+import ReactDOM    from 'react-dom';
+import ViewDoc     from './components/view-doc.jsx';
+import ViewOpenUrl from './components/view-openurl.jsx';
+import Home        from './components/home.jsx';
 
 import { Router, Route, Link, browserHistory } from 'react-router';
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/"  component={Home} />
-    <Route path="/*" component={ViewDoc} />
+    <Route path="/"         component={Home} />
+    <Route path="/openurl*" component={ViewOpenUrl} />
+    <Route path="/*"        component={ViewDoc} />
   </Router>
 ), document.getElementById('istex-viewer'));
