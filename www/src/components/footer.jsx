8a3a424ac9fc729a8c6f4253940887e343fe846e
Stéphane Gully <stephane.gully@gmail.com>
2017-04-26 00:23:31 +0200

    patch version and commit

--- www/src/components/footer.jsx
+++ www/src/components/footer.jsx
@@ -7,7 +7,7 @@ class IstexViewFooter extends React.Component {
         <hr />
         <div className="col-lg-12">
           <p className="muted pull-right">
-            <a href="https://github.com/istex/istex-view">ISTEX VIEW</a> version 2.0.0
+            <a href="https://github.com/istex/istex-view">ISTEX VIEW</a> version 2.0.6
           </p>
         </div>
       </footer>
