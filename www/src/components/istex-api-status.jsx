bff7d866c9acf8c8d3034422424eaa67644869fd
Stéphane Gully <stephane.gully@gmail.com>
2017-05-01 12:21:42 +0200

    better error 500 handling

--- www/src/components/istex-api-status.jsx
+++ www/src/components/istex-api-status.jsx
@@ -7,7 +7,11 @@ class IstexApiStatus extends React.Component {
     this.state = {
       // do not show warning as long as the ajax
       // request is not finished
-      isAvailable: true
+      isAvailable: true,
+
+      // if not available we could have an error Msg to show
+      errorCode: null,
+      errorMsg: '',
     };
   }
 
@@ -20,13 +24,19 @@ class IstexApiStatus extends React.Component {
     }).then(function (config) {
 
       // call istex-api to check if the server is ready
-      fetch(config.istexApiUrl + '/document/?q=*&output=id,ark&sid=istex-view').then(function (response) {
-        self.setState({isAvailable: true});
+      fetch(config.istexApiUrl + '/document/?q=*&output=id&sid=istex-view').then(function (response) {
+        if (response.ok) {
+          self.setState({isAvailable: true});
+          return null;
+        } else {
+          self.setState({isAvailable: false, errorCode: response.status});
+          return response.json();
+        }
+      }).then(function (responseData) {
+        self.setState({errorMsg: responseData._error || ''});
       }).catch(function (err) {
         self.setState({isAvailable: false});
       });
-
-
     });
 
   }
@@ -36,7 +46,8 @@ class IstexApiStatus extends React.Component {
     if (!self.state.isAvailable) {
       return (
         <div className="alert alert-danger" role="alert">
-          ISTEX API is not listening. Please start it to be able to view ISTEX documents.
+          L'API ISTEX est temporairement en panne. Veuillez réessayer plus tard.
+          <br/><small><span className="glyphicon glyphicon-cog" title="Détail technique de l'erreur rencontrée"></span> Erreur {self.state.errorCode} : {self.state.errorMsg}</small>
         </div>
       );
     } else {
